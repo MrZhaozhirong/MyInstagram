4339d919f1e2774636c90fcfd21aef877365f957
MrZhaozhirong <2377871036@qq.com>
2017-01-12 18:56:38 +0800

    About Comments

--- app/src/main/java/com/pixel/myinstagram/adapter/CommentsAdapter.java
+++ app/src/main/java/com/pixel/myinstagram/adapter/CommentsAdapter.java
@@ -2,31 +2,92 @@ package com.pixel.myinstagram.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.pixel.myinstagram.activity.CommentsActivity;
+import com.pixel.myinstagram.R;
+import com.pixel.myinstagram.utils.RoundedTransformation;
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 /**
  * Created by ZZR on 2017/1/12.
  */
 
 public class CommentsAdapter extends RecyclerView.Adapter {
+    private Context context;
+    private int avatarSize;
 
     public CommentsAdapter(Context context) {
+        this.context = context;
+        avatarSize = context.getResources().getDimensionPixelSize(R.dimen.comment_avatar_size);
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        final View view = LayoutInflater.from(context).inflate(R.layout.item_comment, parent, false);
+        return new CommentViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
+        CommentViewHolder holder = (CommentViewHolder) viewHolder;
+        switch (position % 3) {
+            case 0:
+                holder.tvComment.setText("Lorem ipsum dolor sit amet, consectetur adipisicing elit.");
+                break;
+            case 1:
+                holder.tvComment.setText("Cupcake ipsum dolor sit amet bear claw.");
+                break;
+            case 2:
+                holder.tvComment.setText("Cupcake ipsum dolor sit. Amet gingerbread cupcake. Gummies ice cream dessert icing marzipan apple pie dessert sugar plum.");
+                break;
+        }
 
+        Picasso.with(context)
+                .load(R.mipmap.ic_launcher)
+                .centerCrop()
+                .resize(avatarSize, avatarSize)
+                .transform(new RoundedTransformation())
+                .into(holder.ivUserAvatar);
     }
 
+    private int itemsCount = 10;
     @Override
     public int getItemCount() {
-        return 0;
+        return itemsCount;
+    }
+
+    public void updateItems() {
+        itemsCount = 10;
+        notifyDataSetChanged();
+    }
+
+    public void addItem() {
+        itemsCount++;
+        notifyItemInserted(itemsCount - 1);
+    }
+
+
+
+
+
+
+    public static class CommentViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.ivUserAvatar)
+        ImageView ivUserAvatar;
+        @BindView(R.id.tvComment)
+        TextView tvComment;
+
+        public CommentViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
     }
 }
